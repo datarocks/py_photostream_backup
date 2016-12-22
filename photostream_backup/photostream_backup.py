923553d81a8a436b88a6dbb6121ee0ef5efbfedc
datarocks <jim.k.github@gmail.com>
2016-12-21 21:24:25 -0600

    join paths properly

--- photostream_backup/photostream_backup.py
+++ photostream_backup/photostream_backup.py
@@ -2,8 +2,9 @@ import os
 import records
 
 home_dir = os.path.expanduser('~')
-base_photo_stream_dir = home_dir+"/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/" \
-                            "com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams"
+base_photo_stream_dir = os.path.join(home_dir,'Library', 'Containers', 'com.apple.cloudphotosd', 'Data', 'Library',
+                                     'Application Support', 'com.apple.cloudphotosd', 'services',
+                                     'com.apple.photo.icloud.sharedstreams')
 
 def initialize(destination, streams = None, verbose = False):
 
@@ -15,22 +16,6 @@ def initialize(destination, streams = None, verbose = False):
         # TODO: make a real output statement
         print "No streams selected, defaulting to all: " + ' ,'.join(map(str, streams_list))
 
-
-#    raise ArgumentError, "Unable to read destination directory" unless File.readable? File.expand_path(destination)
-#    @destination = File.expand_path(destination)
-#
-#    if streams.nil?
-#      @streams = get_all_ps_names
-#      puts
-#    elsif streams == ['all']
-#      @streams = get_all_ps_names
-#    else
-#      @streams = streams
-#    end
-#
-#    @verbose = verbose
-#  end
-
 def get_sqllite_db_path(photostream_dir):
     """
     This module is basically just to find the sqllite database from the base photostream directory.
@@ -38,35 +23,36 @@ def get_sqllite_db_path(photostream_dir):
     :param photostream_dir:
     :return: sqllite file path
     """
-    share_dir = photostream_dir + "/coremediastream-state/"
+    share_dir = os.path.join(photostream_dir, 'coremediastream-state')
     # now look for the directory in the share dir, and grab the first one
     # based on this answer:
     # http://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
-    # this folder name can probably be figured out programatically
+    # this folder name can probably be figured out programmatically
     model_dir = next(os.walk(share_dir))[1][0]
-    sqlite_path = share_dir + model_dir + "/Model.sqlite"
+    sqlite_path = os.path.join(share_dir, model_dir, "Model.sqlite")
     return sqlite_path
 
 def get_ps_img_uuids(stream_name, photostream_dir):
     sqlite_path = get_sqllite_db_path(photostream_dir)
     db = records.Database('sqlite+pysqlite:///'+sqlite_path)
-
     sql = "SELECT ac.GUID AS 'uuid', ac.photoDate AS 'date' FROM AssetCollections AS ac JOIN Albums AS a ON a.GUID = " \
           "ac.albumGUID WHERE a.name = '%s';" % stream_name
-    rows = db.query(sql)
-    print(rows.as_dict())
+    rows = db.query(sql).as_dict()
+    return rows
 
 def get_ps_album_uuids(stream_name, photostream_dir):
     sqlite_path = get_sqllite_db_path(photostream_dir)
     db = records.Database('sqlite+pysqlite:///'+sqlite_path)
     sql = "SELECT a.GUID AS 'uuid' FROM Albums AS a WHERE a.name = '%s';" % stream_name
-    rows = db.query(sql)
-    print ('albums')
-    print(rows.as_dict())
+    rows = db.query(sql).as_dict()
+    return rows
 
 #initialize(destination=photo_stream_dir)
 
 #get_sqllite_db_path(photo_stream_dir)
 
-get_ps_img_uuids("Sylvia", base_photo_stream_dir)
-get_ps_album_uuids("Sylvia", base_photo_stream_dir)
+photo_folders = get_ps_img_uuids("Sylvia", base_photo_stream_dir)
+print photo_folders
+stream_folder = get_ps_album_uuids("Sylvia", base_photo_stream_dir)
+print('stream(s)')
+print stream_folder
